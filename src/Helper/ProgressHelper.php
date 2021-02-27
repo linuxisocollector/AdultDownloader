8e5e1f238d3475e47bc491ae8f1a9d6233ead585
Linux ISO Collector <linuxisocollector@will-hier-weg.de>
2021-02-27 17:05:32 +0100

    Padding for Progressbars

--- src/Helper/ProgressHelper.php
+++ src/Helper/ProgressHelper.php
@@ -24,17 +24,29 @@ class ProgressHelper {
         $section1 = $output->section();
         $section2 = $output->section();
         $this->section2 = $section2;
-
+        $video_strlen = (strlen(count($videos)));
         $this->progressBar1 = new ProgressBar($section1, count($this->videos));
-        $this->progressBar1->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %message%');
+        $format_videos = '';
+        $padding_video = 0;
+        if($video_strlen < 3) {
+            $padding_video = (6 - $video_strlen *2) / 2;
+        }
+        $format_videos =  str_repeat(' ',$padding_video)." %current%/%max%".str_repeat(' ',$padding_video)." [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %message%";
+        $this->progressBar1->setFormat($format_videos);
         if(count($this->videos) > 0) {
             $this->progressBar1->setMessage($this->videos[0]->getMetadata()->getSceneName());
         }
+
         $this->progressBar2 = new ProgressBar($section2, 100);
-        $this->progressBar2->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %message%');
+        $percentage_padding = $video_strlen - 3;
+        $percentage_padding = $percentage_padding <=0 ? 0 : $percentage_padding;
+        
+        $format_download = str_repeat(' ',$percentage_padding)." %current%/%max%".str_repeat(' ',$percentage_padding)." [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %message%";
+
+        $this->progressBar2->setFormat($format_download);
         // starts and displays the progress bar
         $this->progressBar1->start();
-        $this->progressBar2->setMessage("Starting");
+       // $this->progressBar2->setMessage("Starting");
         $this->progressBar2->start();
 
 
